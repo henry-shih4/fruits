d9f13a80b386b9d61ef3cdde328238264b771f95
henry-shih4 <100496651+henry-shih4@users.noreply.github.com>
2022-11-23 10:28:22 -0600

    header sliding animation
    using keyframes in tailwind config to make animation of header on nav change

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,13 @@ import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
   const [background, setBackground] = useState();
+  const [navAnimation, setNavAnimation] = useState(false);
 
   let location = useLocation();
+
+  useEffect(() => {
+    console.log(navAnimation);
+  });
 
   useEffect(() => {
     if (location.pathname === "/watermelon") {
@@ -22,11 +27,18 @@ export default function Header() {
     <>
       <div
         id="slideIn"
-        className={`h-12 w-full flex justify-start items-center`}
+        className={`h-12 w-full flex justify-start items-center relative z-1`}
       >
         <div
           id="sliderBg"
-          className={`${background} absolute top-0 left-0 w-full h-full -z-10`}
+          className={
+            navAnimation
+              ? `animate-background-slide ${background} absolute top-0 left-0 w-full h-full -z-10`
+              : `${background} absolute top-0 left-0 w-full h-full -z-10`
+          }
+          onAnimationEnd={() => {
+            setNavAnimation(false);
+          }}
         ></div>
         <NavLink
           className={({ isActive }) =>
@@ -44,10 +56,14 @@ export default function Header() {
               ? "justify-center w-[100px] h-full flex items-center p-2 bg-white text-black transition-colors duration-500"
               : "justify-center w-[100px]  p-2 h-full flex items-center text-white"
           }
+          onClick={() => {
+            setNavAnimation(true);
+          }}
           to="/watermelon"
         >
           Watermelon
         </NavLink>
+
         <NavLink
           className={({ isActive }) =>
             isActive
@@ -55,16 +71,22 @@ export default function Header() {
               : "justify-center w-[100px] p-2 h-full flex items-center text-white"
           }
           to="/orange"
+          onClick={() => {
+            setNavAnimation(true);
+          }}
         >
           Orange
         </NavLink>
         <NavLink
           className={({ isActive }) =>
             isActive
-              ? "h-full flex items-center p-2 justify-center w-[100px] bg-white text-black transition-colors duration-500"
-              : "justify-center w-[100px]  p-2 h-full flex items-center text-white"
+              ? "justify-center w-[100px] h-full flex items-center p-2  bg-white text-black transition-colors duration-500"
+              : "justify-center w-[100px] p-2 h-full flex items-center text-white"
           }
           to="/kiwi"
+          onClick={() => {
+            setNavAnimation(true);
+          }}
         >
           Kiwi
         </NavLink>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideIn: {
+          "0%": {
+            transform: "translateX(-100%)",
+          },
+
+          "100%": {
+            transform: "translateX:(0)",
+          },
+        },
+      },
+      animation: {
+        "background-slide": "slideIn 1.5s linear",
+      },
+    },
   },
   plugins: [],
 };
